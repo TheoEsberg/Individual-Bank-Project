8099024f6c219fb6e463f9a20b5ce5e0409445e7
Theo <Theo@DESKTOP-CVJK2UD>
2022-09-28 12:02:00 +0200

    Add project files.

---
+++ Individual Bank Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Individual_Bank_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
